597b48abb464431badd2e05bbe0b332c43efc11d
Stefan N. Cherubin <scherubin@nyu.edu>
2016-02-14 14:46:27 -0500

    More cleanup + documentation

--- Homework/HW2 SCherubin.cpp
+++ Homework/HW2 SCherubin.cpp
@@ -176,7 +176,8 @@ public:
 	}
 };
 
-bool processEvents(SDL_Event* events){
+//Checks to see if somebody presses the close button
+bool windowCloseChecker(SDL_Event* events){
 	bool done = false;
 	if (events->type == SDL_QUIT || events->type == SDL_WINDOWEVENT_CLOSE) { //Toss this entire if statement into processInput function
 		done = true;
@@ -185,7 +186,8 @@ bool processEvents(SDL_Event* events){
 	
 	return done;
 }
-//Update Ball Movement
+
+//Updates Ball Movement
 //KNOWN ISSUE: BALL MAY FAIL TO CHANGE EITHER X OR Y DIRECTION DEPENDING ON WHETHER IT HITS VERTICAL OR HORIZONTAL BARRIERS
 void processBallMovement(Element* ball, float elapsed){
 	
@@ -200,24 +202,18 @@ void processBallMovement(Element* ball, float elapsed){
 }
 
 void collisionDetection(Element* ball, Element* leftPaddle, Element* rightPaddle, float elapsed){
+	//Top Barrier Collision
 	if (ball->getYPos() > 0.79){
 		ball->VDirection *= -1;
-		//ball->incrementYPos(-200.75f * elapsed);
-		//ball->moveMatrix(0.0, ball->getYPos() * elapsed, 0.0);
+		
 	}
+	//Bottom Barrier Collision
 	if (ball->getYPos() < -0.79){
 		ball->VDirection *= -1;
-		//ball->incrementYPos(-200.75f * elapsed);
-		//ball->moveMatrix(0.0, ball->getYPos() * elapsed, 0.0);
+		
 	}
-	/*if (ball->getXPos() > 1.6){
-	ball->HDirection *= -1;
-	}*/
-
-	/*if (ball->getXPos() <- 1.6){
-	ball->HDirection *= -1;
-	}*/
-	//Left Paddle Collison
+	
+	//Left Paddle Collision
 	if (((ball->getXPos() - ball->width * 0.5) < leftPaddle->getXPos()) && ((ball->getYPos() + ball->height * 0.5) > (leftPaddle->getYPos() - leftPaddle->height * 0.5))
 		&& ((ball->getYPos() - ball->height * 0.5) < (leftPaddle->getYPos() + leftPaddle->height * 0.5))){
 		ball->HDirection *= -1;
@@ -240,14 +236,14 @@ void collisionDetection(Element* ball, Element* leftPaddle, Element* rightPaddle
 		ball->setYPos(0.0);
 		ball->HDirection *= -1;
 		leftPaddle->updateScore();
-		//SDL_GL_SwapWindow(displayWindow);
 	}
 
 }
-//Keeps track of score.
+
+//Keeps track of score. Equal score will result in purple background.
+//Screen changes colors to indicate who is leading.
 void scoreBoard(Element* leftPaddle, Element* rightPaddle, Element* ball){
 	if (leftPaddle->getScore() == rightPaddle->getScore()){
-		//glClearColor(redFilter, 0.0f, blueFilter, 1.0f);
 		redFilter = 0.5f;
 		blueFilter = 0.5f;
 	}
@@ -285,59 +281,45 @@ void scoreBoard(Element* leftPaddle, Element* rightPaddle, Element* ball){
 		}
 	}
 	
-	if (leftPaddle->getScore() == 4 || rightPaddle->getScore() == 4){
-		//ball->moveMatrix(0.0, 0.0, 0.0);
+	if (leftPaddle->getScore() == 4 || rightPaddle->getScore() == 4){ //GAME OVER	
 		ball->identityMatrix();
-		ball->scaling(0.0, 0.0, 0.0);
+		ball->scaling(0.0, 0.0, 0.0); //Ball disappears
 		if (leftPaddle->getScore() == 4){
 			///PLAYER 1 WINS
 			redFilter = 1.0f;
 			blueFilter = 0.0f;
-			//glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-			//glClear(GL_COLOR_BUFFER_BIT);
-			//glClear(GL_COLOR_BUFFER_BIT);
+			
 		}
 		else {
 			//PLAYER 2 WINS
 			redFilter = 0.0f;
 			blueFilter = 1.0f;
-			//glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-			//glClear(GL_COLOR_BUFFER_BIT);
-			//glClear(GL_COLOR_BUFFER_BIT);
-			//SDL_GL_SwapWindow(displayWindow);
 		}
 	}
 	
 }
 
+//Processes User Input and Updates screen
 void processInput(Element* element, Element* element2, float elapsed){
 
 	        const Uint8 *keys = SDL_GetKeyboardState(nullptr);
 			const Uint8 *keys2 = SDL_GetKeyboardState(nullptr);
 			float yPos = element->getYPos();
 			float yPos2 = element2->getYPos();
-			//float yPos3 = element3->getYPos(); 
-			//element3->setYPos(0.3 * elapsed);
-			//if (keys[SDL_SCANCODE_LEFT] || keys[SDL_SCANCODE_A]){
-			//	smileyX -= elapsed * 2.0f;
-			//	//ghostX -= elapsed * 1.0f;
-			//}
-			//if (keys[SDL_SCANCODE_RIGHT] || keys[SDL_SCANCODE_D]){
-			//	smileyX += elapsed * 2.0f;
-			//}
-			if (keys[SDL_SCANCODE_W]){
+	
+			if (keys[SDL_SCANCODE_W]){ 
 				element->incrementYPos(elapsed * 2.0f);
 			}
 			if (keys[SDL_SCANCODE_S]){
-				//smileyY -= elapsed * 2.0f;
+				
 				element->incrementYPos(elapsed * -2.0f);
 			}
 			if (keys2[SDL_SCANCODE_UP]){
-				//ghostY += elapsed * 2.0f;
+				
 				element2->incrementYPos(elapsed * 2.0f);
 			}
 			if (keys2[SDL_SCANCODE_DOWN]){
-				//ghostY -= elapsed * 2.80f;
+				
 				element2->incrementYPos(elapsed * -2.0f);
 			}
 			if (yPos > 0.79){
@@ -352,63 +334,14 @@ void processInput(Element* element, Element* element2, float elapsed){
 			if (yPos2 < -0.79){
 				element2->setYPos(-0.78);
 			}
-			/*if (yPos3 > 0.79){
-				element3->setYPos(0.78);
-			}*/
-						/*if (ghostY > 0.79){
-				ghostY = 0.79;
-			}
-			if (ghostY < -0.79){
-				ghostY = -0.79;
-			}*/
-			//float units_x = (pixel_x / x_resolution) * ortho_width ) - ortho_width / 2.0;
+
+
+			
 }
 
-void DrawText(ShaderProgram *program, int fontTexture, string text, float size, float spacing) {
-	float texture_size = 1.0 / 16.0f;
-	vector<float> vertexData;
-	vector<float> texCoordData;
-
-	for (int i = 0; i < text.size(); i++) {
-		float texture_x = (float)(((int)text[i]) % 16) / 16.0f;
-		float texture_y = (float)(((int)text[i]) / 16) / 16.0f;
-		vertexData.insert(vertexData.end(), {
-			((size + spacing) * i) + (-0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
-			((size + spacing) * i) + (0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (0.5f * size), -0.5f * size,
-			((size + spacing) * i) + (0.5f * size), 0.5f * size,
-			((size + spacing) * i) + (-0.5f * size), -0.5f * size,
-		});
-		texCoordData.insert(texCoordData.end(), {
-			texture_x, texture_y,
-			texture_x, texture_y + texture_size,
-			texture_x + texture_size, texture_y,
-			texture_x + texture_size, texture_y + texture_size,
-			texture_x + texture_size, texture_y,
-			texture_x, texture_y + texture_size,
-		});
-	}
-	glUseProgram(program->programID);
-
-	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertexData.data());
-	glEnableVertexAttribArray(program->positionAttribute);
-	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoordData.data());
-	glEnableVertexAttribArray(program->texCoordAttribute);
-	glBindTexture(GL_TEXTURE_2D, fontTexture);
-	glDrawArrays(GL_TRIANGLES, 0, text.size() * 6);
-
-	glDisableVertexAttribArray(program->positionAttribute);
-	glDisableVertexAttribArray(program->texCoordAttribute);
-}
-int main(int argc, char *argv[]){
-	//cout << "nope" << endl;
+//Creates view window
+void setup(){
 	SDL_Init(SDL_INIT_VIDEO);
-	//SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
-	//SDL_JoystickOpen is passed the joystick index
-	//SDL_Joystick * playerOneController = SDL_JoystickOpen(0);
-	//In while loop, clean up for each open joystick
-	//SDL_JoystickClose(playerOneController);
 	int x_resolution = 800;
 	int y_resolution = 600;
 	displayWindow = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, x_resolution, y_resolution, SDL_WINDOW_OPENGL);// <-OUTER BOUND
@@ -419,16 +352,26 @@ int main(int argc, char *argv[]){
 	glewInit();
 #endif
 	glViewport(0, 0, x_resolution, y_resolution);//<- INNER BOUND
+}
+
+//Clears screen. Setting background color is optional.
+void clearScreen(){
+	glClearColor(redFilter, greenFilter, blueFilter, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+//Initalizing entities, setting things up, etc.
+int main(int argc, char *argv[]){
+	setup();
 	ShaderProgram program(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
 
-	Matrix projectionMatrix, leftProjectionMatrix;
-	Matrix modelMatrix, leftModelMatrix;
-	Matrix viewMatrix, leftViewMatrix;
+	Matrix projectionMatrix;
+	Matrix modelMatrix;
+	Matrix viewMatrix;
 
 	Element ball(modelMatrix, projectionMatrix, viewMatrix);
-	//Element leftPaddle(leftModelMatrix, leftProjectionMatrix, leftViewMatrix);
-	Element leftPaddle(modelMatrix, projectionMatrix, viewMatrix);
-	Element rightPaddle(modelMatrix, projectionMatrix, viewMatrix);
+	Element leftPaddle(modelMatrix, projectionMatrix, viewMatrix);//Player1
+	Element rightPaddle(modelMatrix, projectionMatrix, viewMatrix);//Player2
 	
 	SDL_Event event;
 	bool done = false;
@@ -438,24 +381,16 @@ int main(int argc, char *argv[]){
 		float ticks = (float)SDL_GetTicks() / 1000.0f; float elapsed = ticks - lastFrameTicks; lastFrameTicks = ticks;
 
 		while (SDL_PollEvent(&event)) {
-			done = processEvents(&event);
+			done = windowCloseChecker(&event);
 		}
 		
-
-		glClearColor(redFilter, greenFilter, blueFilter, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		clearScreen();
 		
 		//ball
 		ball.setMatrices(&program);
 		ball.setOrthoProjection();
 		ball.setWidthAndHeight(vertices2);
 		ball.render(&program, vertices2, texCoords);
-		//ball.setYPos(0.25 * elapsed);
-		//ball.incrementYPos(0.25 * elapsed);
-		//ball.setYPos(0.25);
-		//ball.setYPos(0.15);
-		//ball.setYPos(0.25);
-		//ball.moveMatrix(0.0 , ball.getYPos() * elapsed, 0.0);
 		
 		//leftPaddle, Player 1, controlled by "W" and "S" keys
 		leftPaddle.setMatrices(&program);
@@ -478,49 +413,25 @@ int main(int argc, char *argv[]){
 		rightPaddle.moveMatrix(rightPaddle.getXPos() , 0.0, 0.0);
 		rightPaddle.moveMatrix(0.0, rightPaddle.getYPos(), 0.0);
 		rightPaddle.scaling(0.2, 0.4, 1.0);
+
+
 		//Processing movement
 		processInput(&leftPaddle, &rightPaddle, elapsed);
 		processBallMovement(&ball, elapsed);
+
 		//Collision Detection
 		collisionDetection(&ball, &leftPaddle, &rightPaddle, elapsed);
+
 		//Scoring
 		scoreBoard(&leftPaddle, &rightPaddle, &ball);
 		
-		//DrawText(&program, 2, "?", 2, 1);
-		//ball.identityMatrix();
-		//ball.scaling(0.1, 0.5, 1.0);
-		//modelMatrix2.identity();
-
-
-		//modelMatrix.Translate(-3.0, 0.0, 0.0);
-		//modelMatrix2.Translate(ghostX, 0.0f, 0.0f);
-		//moveY += elapsed;
-		
-		//glBindTexture(GL_TEXTURE_2D, emojiTexture);
-		//smileyX += elapsed * 0.5f;
 
 		angle += elapsed;
 
-		//mod3.Translate(0.0f, moveY, 0.0f);
-
-
-		//modelMatrix.Translate(0.5f * elapsed, 0.0f, 0.0f);
-		//modelMatrix2.Translate(0.5f, 0.0, 0.0f);
-		//modelMatrix2.Rotate(45.0 * (3.1415926 / 180.0));
-		//if (smileyX > 2.0 || smileyX < -2.0 || smileyY > 1.0 || smileyY < -1.0) {
-		//	//modelMatrix2.Rotate(25.0 * (3.1415926 / 180.0));
-		//	//modelMatrix2.Rotate(25.0 * (3.1415926 / 180.0));
-		//	//mod3.Rotate(25.0 * (3.1415926 / 180.0));
-		//	
-		//}
-
-		
-		//todo: collision detection, keeping score, organize code
-		//SDL_JoystickClose(playerOneController);
-
+	
 		SDL_GL_SwapWindow(displayWindow);
 	}
-	//SDL_JoystickClose(playerOneController);
+	
 
 	SDL_Quit();
 	return 0;
